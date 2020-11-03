efba3731fdd2a02175a629efef33d4f16795e0a6
pekeramazan <pekeramazan@gmail.com>
2020-11-04 00:11:44 +0300

    UnitOfWorks interface oluşturulması

---
+++ NLayerProject.Core/UnitOfWorks/UnitOfWorks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerProject.Core.Repositories;
+
+namespace NLayerProject.Core.UnitOfWorks
+{
+    public interface UnitOfWorks
+    {
+        IProductRepository Products { get; }
+        ICategoryRepository Categories { get; }
+
+        Task CommitAsync();
+        void Commit();
+    }
+}
