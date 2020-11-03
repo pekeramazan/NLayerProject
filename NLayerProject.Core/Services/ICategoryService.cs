d4775fa0427aa8841a09409f4cddf185fa1a25cc
pekeramazan <pekeramazan@gmail.com>
2020-11-04 00:09:37 +0300

    service interfacelerinin oulşturulması

---
+++ NLayerProject.Core/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerProject.Core.Models;
+
+namespace NLayerProject.Core.Services
+{
+    public interface ICategoryService : IService<Category>
+    {
+        Task<Category> GetWithProductsByIdAsync(int categoryId);
+
+        //Category özgü methodlarınız varsa burada tanımlayabilirsiniz.
+    }
+}
---
+++ NLayerProject.Core/Services/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerProject.Core.Models;
+
+namespace NLayerProject.Core.Services
+{
+    public interface IProductService : IService<Product>
+    {
+        Task<Product> GetWithCategoryByIdAsync(int productId);
+
+        //  bool ControlInnerBarcode(Product product);
+    }
+}
